06969797403c6423f8c0f6fabc1525046fb7ffa9
DanBlechner <danblechner@gmail.com>
2014-07-03 19:12:53 +0300

    initial commit of photohunt for android
    source: goole demo code:
    https://github.com/googleplus/gplus-photohunt-client-android

---
+++ client-android/src/com/google/plus/samples/photohunt/model/UploadUrl.java
@@ -0,0 +1,8 @@
+package com.google.plus.samples.photohunt.model;
+
+public class UploadUrl {
+    /**
+     * The upload URL returned within the JSON response from the server.
+     */
+    public String url;
+}
